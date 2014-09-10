49ef24e8a79462c071ea59fb63f1e2ae1e252db9
Tiago Coimbra Vesper <tiago.vesper@gmail.com>
2014-09-09 23:03:40 -0300

    Modificaçoes na classe produto-lista

--- produto-lista.php
+++ produto-lista.php
@@ -1,10 +1,20 @@
 <?php include("cabecalho.php")?>
 <?php include("conecta.php");
 
-$resultado = mysqli_query($conexao,'select * from produto');
+function listarProdutos($conexao){
+    $produtos = array();
+    $resultado = mysqli_query($conexao,'select * from produto');
 
-while($produto = mysqli_fetch_assoc($resultado)){
-    echo $produto['nome'] . '</br>';
+    while($produto = mysqli_fetch_assoc($resultado)){
+        array_push($produtos,$produto);
+    }
+
+    return $produtos;
+}
+
+$produtos = listarProdutos($conexao);
+foreach($produtos as $produto) {
+    echo $produto['nome'];
 }
 
 ?>
