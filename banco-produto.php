bb1b5dfee938015995025f5ae45e360ab9b57295
tiagovesper <tiago.vesper@gmail.com>
2014-09-17 09:40:54 -0300

    Feito algumas mudanças de melhoria do código, e criado as classes banco-produto e remove-produto.

--- banco-produto.php
+++ banco-produto.php
@@ -2,11 +2,9 @@
 function listarProdutos($conexao){
     $produtos = array();
     $resultado = mysqli_query($conexao,'select * from produto');
-
     while($produto = mysqli_fetch_assoc($resultado)){
     array_push($produtos,$produto);
     }
-
     return $produtos;
 }
 
